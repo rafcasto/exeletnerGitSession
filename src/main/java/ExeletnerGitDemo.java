d865d126e5d8adff4e4beba15d30468b1a871bc7
rafcasto <rafcasto@gmail.com>
2021-02-27 15:46:00 +1300

    Update ExeletnerGitDemo.java

--- src/main/java/ExeletnerGitDemo.java
+++ src/main/java/ExeletnerGitDemo.java
@@ -2,6 +2,7 @@ public class ExeletnerGitDemo
 {
         public static void main(String [] args)
         {
+            //Conflic comment
             System.out.println("Hello world");
         }
 }
